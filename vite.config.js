cd1b88c0dcfcdd0b90fe8056c157aeb40fa4f123
Sourav <sourav.khilari.it26@heritageit.edu.in>
2025-04-20 00:00:18 +0530

    `Update import paths and vite config for React Router and Tailwind CSS plugins`

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),], 
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: process.env.VITE_BASE_PATH || "/Ev_Dashboard", 
-})
+  plugins: [react(),tailwindcss(),],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    fs: {
+      strict: true,
+    },
+  },
+});
