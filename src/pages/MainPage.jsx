b3594ca8ab43f56a14d5e0c09ae03a5cd2bdcbbb
Sourav <sourav.khilari.it26@heritageit.edu.in>
2025-04-20 14:21:18 +0530

    `Reverted import statement and added newline at end of file`

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,30 @@
+// import React from 'react';
+// import Navbar from '../components/NavBar.jsx';
+// import { Outlet } from "react-router-dom";
+// import  {DataProvider}  from "../context/DataContext.jsx";
+// import Footer from "../components/Footer.jsx";
+
+// function MainPage() {
+//   return (
+//     <DataProvider>
+//       <div className="flex flex-col min-h-screen w-screen">
+//         <Navbar />
+//         <div className="flex-1 mt-16 px-4 md:px-8 py-4 w-full" >
+//           <Outlet />
+//         </div>
+
+//         <Footer />
+//       </div>
+//     </DataProvider>
+//   );
+// }
+// export default MainPage;
+
+
 import React from 'react';
 import Navbar from '../components/NavBar.jsx';
 import { Outlet } from "react-router-dom";
-import  {DataProvider}  from "../context/DataContext.jsx";
+import { DataProvider } from "../context/DataContext.jsx";
 import Footer from "../components/Footer.jsx";
 
 function MainPage() {
@@ -9,7 +32,7 @@ function MainPage() {
     <DataProvider>
       <div className="flex flex-col min-h-screen w-screen">
         <Navbar />
-        <div className="flex-1 mt-16 px-4 md:px-8 py-4 w-full" >
+        <div className="flex-1 mt-16 px-4 md:px-8 py-4 w-full pt-16 md:pt-0">
           <Outlet />
         </div>
 
@@ -18,4 +41,5 @@ function MainPage() {
     </DataProvider>
   );
 }
+
 export default MainPage;
